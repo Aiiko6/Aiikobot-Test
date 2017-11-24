e4a2b0514e3dd5ab4da60936ba5918e064b1967f
Aiiko6 <33965737+Aiiko6@users.noreply.github.com>
2017-11-24 20:28:43 +0100

    Update names.js

--- names.js
+++ names.js
@@ -1,32 +1,26 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const discord = require('discord.js');
+const client = new discord.Client();
+const prefix = '!';
 
-const token = "MzY4NDc3ODU2NDk5NjMwMDgx.DPn_gw.XuE3geDe532nCv1jycAbWtzN5qU";
-var prefix = "'";
-var mention = "<@1930903359700619264>";
-var memberCount = client.users.size;
-var servercount = client.guilds.size;
 
 client.on("ready", () => {
-	var servers = client.guilds.array().map(g => g.name).join(',');
-	console.log("--------------------------------------");
-console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+  var servers = client.guilds.array().map(g => g.name).join(',');
+  console.log("--------------------------------------");
+  
+  client.user.setGame("#test", "http://twitch.tv/highwaytraffic"); //Joue à ...
 });
 
-client.on('message', message => {
-	if (message.content === ("test")){
-	message.reply('test !');
-} else if (message.content === ("bonjour")){
-	message.reply('bonjour à toi ');
-} else if(message.content.startsWith('!botname')){
-	client.user.setUsername(message.content.substr(9));
-} else if (message.content === "!stats") {
-	let m = " ";
-	m += 'Il y a actuellement  ${message.guild.channels.size} channels sur ce serveurs \n';
-	m += 'je suis en compagnie de ${message.guild.members.size} membres';
-	m += 'je suis présent dans ${client.guild.size} serveurs \n';
-	message.author.sendMessage(m).catch(console.log); 
+client.on('message', message =>{
+  if(message.content.startsWith('#test')){
+      message.reply('yes');
+     }
+});
+
+//identification
+var dt = process.env.DISCORD_TOKEN || process.argv[2];
+
+if (!dt) {
+    console.log('votretoken');
 }
-});
 
-client.login(token)
+client.login(dt);
